588d4bdc76a3425a81ce882f85fb06375fedfa5d
Sina Zaker <34570936+cnazk@users.noreply.github.com>
2018-12-29 22:35:20 +0330

    Update PulleyViewController.swift

--- PulleyLib/PulleyViewController.swift
+++ PulleyLib/PulleyViewController.swift
@@ -821,11 +821,13 @@ open class PulleyViewController: UIViewController, PulleyDrawerViewControllerDel
             
             drawerScrollView.addSubview(drawerShadowView)
             
+            /*
             if let drawerBackgroundVisualEffectView = drawerBackgroundVisualEffectView
             {
                 drawerScrollView.addSubview(drawerBackgroundVisualEffectView)
                 drawerBackgroundVisualEffectView.layer.cornerRadius = drawerCornerRadius
             }
+            */
             
             drawerScrollView.addSubview(drawerContentContainer)
             
